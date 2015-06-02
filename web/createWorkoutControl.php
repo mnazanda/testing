cd90464e6b9ab2d05b645444409c62138e897033
mnazanda <mnazanda@gmail.com>
2015-06-01 19:26:07 -0700

    Updated CSS file

--- web/createWorkoutControl.php
+++ web/createWorkoutControl.php
@@ -9,7 +9,7 @@ $connect=pg_connect("host=ec2-107-21-114-132.compute-1.amazonaws.com port=5432 d
 $username = $_SESSION["userName"];
 
  pg_query($connect,"INSERT INTO workouts (workoutDate,  title, location, numOfReserves ,spotsLeft, author, description, startTime, endTime)
-        VALUES ('$_POST[date]', '$_POST[title]', '$_POST[location]', '$_POST[numOfReserves]', '$_POST[numOfReserves]', '$username', '$_POST[description]', '$_POST[startTime]', '$_POST[endTime]');");
+        VALUES ('$_POST[date]', '$_POST[title]', '$_POST[location]', '$_POST[numOfReserves]', '$_POST[numOfReserves]', '$username', '$_POST[description]', '$_POST[startTime]', '$_POST[endTime]')");
  
 $connect->close();
 
