77ad1e92442d9d9310c166a51fc2481b8acabfca
mnazanda <mnazanda@gmail.com>
2015-06-08 11:49:39 -0700

    changing header

--- web/createAccountControl.php
+++ web/createAccountControl.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Location: Index.php');
+header('Location: index.php');
 
 $connect=pg_connect("host=ec2-107-21-114-132.compute-1.amazonaws.com port=5432 dbname=d6ad8doip7s4vu user=cmcevirzzwpuze password=z7Cu5bKWj8CzZXf3OlSV-Mg90n")
     or die("Could Not Connect");
